8ef74f9f4c1e694c3de5e173034b0588d94718d5
Kristen <kristen.masta@gmail.com>
2019-04-13 20:08:49 -0400

    init commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+
+
+var numberOfWins = document.getElementById("numberOfWinsText");
+var numberOfGuesses = document.getElementById("numberofGuessesText");
+var lettersAlreadyGuessed = document.getElementById("lettersAlreadyGuessedText");
+
+//song index: 
+//   [0] wonderwall - oasis 
+//   [1] i want you back- n'sync
+//   [2] sexandcandy -marcy playground
+//   [3] tubthumping -chumbawumba
+//   [4] friday im in love - the cure
+//   [5] the sign - ace of base
+//   [6] lovefool - the cardigans
+//   [7] kiss from a rose - seal
+//   [8] truly madly deeply - savage garden
+//   [9] my heart will go on - celine dion
+//   [10] all my life - KC & Jo Jo
+//   [11] Baby one more time - Britney Spears
+//   [12] Girl on TV - LFO
+
+var songArray = ["Wonderwall", "I Want You Back", "Sex and Candy", "Tubthumping", "Friday Im In Love", "The Sign", "Love Fool", "Kiss from a Rose", 
+"Truly Madly Deeply", "My Heart Will Go On", "All My Life", "Baby One More Time", "Girl On TV"];
+
+var artistArray = ["Oasis", "N'Sync", "Marcy Playground", "Chumbawumba", "The Cure", "Ace of Base", "The Cardigans", "Seal",
+ "Savage Garden", "Celine Dion", "KC & Jo Jo", "Britney Spears", "LFO"];
+
+var songAudioFile = [wonderwall.mp3, iwantyouback.mp3, sexandcandy.mp3, tubthumping.mp3, fridayiminlove.mp3, thesign.mp3, lovefool.mp3, kissfromarose.mp3,
+trulymadlydeeply.mp3, myheartwillgoon.mp3, allmylife.mp3, babyonemoretime.mp3, girlontv.mp3];
+
+
+
+
+
+
+
+
+
+
+];
+
+
+function wordAsDashes();
+
