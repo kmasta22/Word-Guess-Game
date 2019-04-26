fe2799ecabffd83e0de0571b3fea22305f1bfca0
Kristen <kristen.masta@gmail.com>
2019-04-26 10:59:45 -0400

    almost done got underscores to work now im stuck on the restart game part

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,5 @@
 
 
-
-
-//song index: 
-//   [0] wonderwall - oasis 
-//   [1] i want you back- n'sync
-//   [2] sexandcandy -marcy playground
-//   [3] tubthumping -chumbawumba
-//   [4] friday im in love - the cure
-//   [5] the sign - ace of base
-//   [6] lovefool - the cardigans
-//   [7] kiss from a rose - seal
-//   [8] truly madly deeply - savage garden
-//   [9] my heart will go on - celine dion
-//   [10] all my life - KC & Jo Jo
-//   [11] Baby one more time - Britney Spears
-//   [12] Girl on TV - LFO
-
 document.addEventListener('DOMContentLoaded', function(){
     
 
@@ -39,14 +22,17 @@ var numberOfGuessesElement = document.getElementById("numberOfGuessesText");
 var lettersAlreadyGuessed = document.getElementById("lettersAlreadyGuessedText");
 
 
-//set number of guesses remaining to 15 at beginning of a game
+var wins = 0;
+var letterAsLowercase = 0;
+var letter = 0;
+var underscoreArray = [];
+var lettersAlreadyGuessedArray = [];
 var numberofGuessesRemaining = 15;
-
 numberOfGuessesElement.innerHTML = "Number of Guesses Remaining: " + numberofGuessesRemaining;
 
 
 //find a random song from the songArray and save it as randomSong
-var randomSong = songArray[Math.floor(Math.random()*songArray.length)]
+var randomSong = songArray[Math.floor(Math.random()*songArray.length)];
 
 //split the randomSong into an array of characters
 var wordArray = randomSong.split("");
@@ -57,12 +43,8 @@ var indexOfRandomSong = songArray.indexOf(randomSong);
 //gives the matching artist name for the song that is at the index of random song
 var matchingArtistName = artistArray[indexOfRandomSong];
 
-//empty array to push underscores onto
-var underscoreArray = [];
-
-//empty array for letters already guessed
-
-var lettersAlreadyGuessedArray = [];
+//find the artist name element and change its content to the matching Artist name for the random song generated at page load
+artistNameElement.innerHTML = (matchingArtistName);
 
 //testing
 console.log(indexOfRandomSong);
@@ -72,99 +54,135 @@ console.log(wordArray);
 
 
 
-//find the artist name element and change its content to the matching Artist name for the random song generated at page load
-artistNameElement.innerHTML = (matchingArtistName);
-
-//loop goes for as long as the wordArray
+//createUnderscores function has a For Loop that runs for the length of the wordArray
 //it takes each index of the wordArray which is random song that was split into characters and tests whether
 //the character at each index is a space or a letter
 //if it is a space it pushes a SPACE into the underscore array that appears on the screen
 //if it is not a space then it must be a letter so it pushes an underscore
 //after it pushes the space or letter onto the array it uses the join function to make it into a string at the songNameElement on the screen
-for (i=0; i < wordArray.length; i++){
 
-        var letterAsLowercase = wordArray[i].toLowerCase();
 
-        console.log(letterAsLowercase);
 
-        if(letterAsLowercase === " "){
+function createUnderscores(){
 
-            console.log("this is a space");
-            underscoreArray.push(" ");
-            songNameElement.innerHTML = underscoreArray.join("");
+
+            for (i=0; i < wordArray.length; i++){
+
+            letterAsLowercase = wordArray[i].toLowerCase();
+
+
+             if(letterAsLowercase === " "){
+
+                     underscoreArray.push(" ");
+                     songNameElement.innerHTML = underscoreArray.join("");
             
 
-        }
+              }
 
-        else{
+            else{
 
-            console.log("key is equal to letter")
-            underscoreArray.push("_");
-            songNameElement.innerHTML = underscoreArray.join("");
+                     underscoreArray.push("_");
+                     songNameElement.innerHTML = underscoreArray.join("");
             
 
-        }
-                   
+              }
+        }           
 }
 
-//capture letter press
-document.onkeyup = function (event) {
 
-letter = event.key;
-letter = letter.toLowerCase();
+        //function captures keypress-saves it in letter variable-then pushes letter onto array and shows it on screen
+        document.onkeyup = function (event) {
 
 
-//push the letter pressed onto an array for lettersAlreadyGuessed and then print the array to the screen
-lettersAlreadyGuessedArray.push(letter);
-lettersAlreadyGuessed.innerHTML = "Letters Already Guessed: " + lettersAlreadyGuessedArray.join("");
+                letter = event.key;
+                letter = letter.toLowerCase();
+                isLetterCorrect(letter);
 
 
+                lettersAlreadyGuessedArray.push(letter);
+                lettersAlreadyGuessed.innerHTML = "Letters Already Guessed: " + lettersAlreadyGuessedArray.join("");
 
+                if(numberofGuessesRemaining <= 1 ){
+
+                    alert("Oops you've downloaded a virus");
+                
+                    restartGame();
+                
+                }
+                
+                else {
+                    numberofGuessesRemaining--;
+                    numberOfGuessesElement.innerHTML = "Number of Guesses Remaining: " + numberofGuessesRemaining;
+                        
+                }
+                
+
+        }
+
+        
 //AFTER KEY PRESS loop through word array, convert each letter to lowercase
 //then check to see if the letter at the current index of the array equals the letter saved on the key press
 //if it equals it then use the function splice to replace the underscore at the current index (i) of the loop
 //if it does not equal then reduce number of guesses remaining
-    for (i=0; i < wordArray.length; i++){
+  function isLetterCorrect(letter){
 
-        letterAsLowercase = wordArray[i].toLowerCase();
+                for (i=0; i < wordArray.length; i++){
+
+                    letterAsLowercase = wordArray[i].toLowerCase();
         
-            
 
                 
-            if (letter == letterAsLowercase){
+                        if (letter == letterAsLowercase){
 
-                     underscoreArray.splice(i, 1, letter);
+                                underscoreArray.splice(i, 1, letter);
 
-                     songNameElement.innerHTML = underscoreArray.join("");
-
-
+                                songNameElement.innerHTML = underscoreArray.join("");
+                                
                      
-                 }
-       
-       
-        else {
+                          }
 
-                numberofGuessesRemaining = numberofGuessesRemaining - 1;
-                numberOfGuessesElement.innerHTML = "Number of Guesses Remaining: " + numberofGuessesRemaining;
+                        var array1 = underscoreArray.toString();
+                        var array2 = wordArray.toString();
+                        array2 = array2.toLowerCase();
+            
+                        if (array1 == array2){
+                           
+                                 alert("You win!");
+                                 wins++;
+                                 //change text of number of wins
+                                 numberOfWinsElement.innerHTML = wins;
 
-                if(numberOfGuessesRemaining = 0){
-
-                    alert("Oops, you've downloaded a virus.");
-
+                                 restartGame();
+                         }
                     }
+       
+                     
+    }
 
-                
-
-
-             }
-
-
-        
+    function restartGame(){
 
 
-        }
+        numberofGuessesRemaining = 15;
+        numberOfGuessesElement.innerHTML = "Number of Guesses Remaining: " + numberofGuessesRemaining;
+
+        randomSong = songArray[Math.floor(Math.random()*songArray.length)];
+        wordArray = randomSong.split("");
+        indexOfRandomSong = songArray.indexOf(randomSong);
+        matchingArtistName = artistArray[indexOfRandomSong];
+        underscoreArray = [];
+        lettersAlreadyGuessedArray = [];
+        artistNameElement.innerHTML = (matchingArtistName);
+        lettersAlreadyGuessed.innerHTML = "Letters Already Guessed: " ;
+        numberOfWinsElement.innerHTML = "Wins: " + wins;
+
+        createUnderscores();
 
     }
+
+        
+    restartGame();
+    
+        
 
 
 
